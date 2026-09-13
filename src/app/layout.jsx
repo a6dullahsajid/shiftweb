de5d4ba6173413c973537465450fc5dc6acc7e5a
Abdullah Sajid <abdullahsajid1029@gmail.com>
2026-09-13 09:49:42 +0530

    fix: og-image for /project pages

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -33,6 +33,31 @@ export const metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      },
+      {
+        url: "/favicon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
   openGraph: {
     type: "website",
     url: "/",
